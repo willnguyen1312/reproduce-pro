b3c613a92a7f5efc1d9089df425c4774cb2b9615
Nam Nguyen <nam.nguyen.node@gmail.com>
2024-01-02 21:19:18 -0400

    It works fine 🥹

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,12 @@ import {
 } from "@apollo/client";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  Link,
+  Outlet,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
 import { App } from "./App.tsx";
 
 import { worker } from "./mocks/browser";
@@ -19,11 +25,50 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
+export const Root = () => {
+  return (
+    <main>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </main>
+  );
+};
+
+export const About = () => {
+  return <h1>About</h1>;
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <App />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  },
+]);
+
 worker.start().then(() => {
   ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
       <ApolloProvider client={client}>
-        <App />
+        <RouterProvider router={router} />
       </ApolloProvider>
     </React.StrictMode>
   );
